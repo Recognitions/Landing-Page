2a40954fe0ea31c7e5b69570f03a98d2483c3236
Recognitions <102226234+Recognitions@users.noreply.github.com>
2022-11-26 23:11:57 -0300

    header

--- components/header.php
+++ components/header.php
@@ -1,14 +1,13 @@
-<?php 
-    $pages = [
-        "home",
-        "contact",
-        "about"
-    ]
-?>
-
 <header>
+    <span></span>
     <form method="post">
         <?php 
+            $pages = [
+                "home",
+                "contact",
+                "about"
+            ];
+
             for($i = 0; $i < count($pages); $i++){
                 $value = $pages[$i];
                 echo '<input type="submit" name="page" value='.$value.'>';
