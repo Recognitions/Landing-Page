b16458cb23f92d009fca12e7189b0ee8b899ea9f
Recognitions <102226234+Recognitions@users.noreply.github.com>
2022-11-26 22:29:40 -0300

    pagination

--- components/header.php
+++ components/header.php
@@ -1,3 +1,18 @@
+<?php 
+    $pages = [
+        "home",
+        "contact",
+        "about"
+    ]
+?>
+
 <header>
-    header
+    <form method="post">
+        <?php 
+            for($i = 0; $i < count($pages); $i++){
+                $value = $pages[$i];
+                echo '<input type="submit" name="page" value='.$value.'>';
+            }
+        ?>
+    </form>
 </header>
--- index.php
+++ index.php
@@ -5,11 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page - Netflix</title>
+    <link rel="stylesheet" href="./styles/global.css">
+    <link rel="stylesheet" href="./styles/header.css">
+    <link rel="stylesheet" href="./styles/footer.css">
 </head>
 <body>
     <?php include_once("./components/header.php"); ?>
     <main>
-        
+        <?php
+            if (isset($_POST['page'])) {
+                include_once("./pages/".$_POST['page'].".php");
+            } else {
+                include_once("./pages/default.php");
+            }
+        ?>
     </main>
     <?php include_once("./components/footer.php"); ?>
 </body>
