5d2446f2da1b69976557d73a48fb7a094fc8201b
Recognitions <102226234+Recognitions@users.noreply.github.com>
2022-11-26 23:57:27 -0300

    default

--- pages/default.php
+++ pages/default.php
@@ -1,1 +1,14 @@
-default
+<section class="default">
+    <span>
+        Assista o que quiser, onde quiser.
+    </span>
+</section>
+<section class="default-2">
+    <div class="default-2-text">
+        <h2>Aproveite na TV.</h2>
+        <label>Assista em Smart TVs, PlayStation, Xbox,<br> Chromecast, Apple TV, aparelhos de Blu-ray e<br> outros dispositivos.</label>
+    </div>
+    <div class="default-2-image">
+        <div class="default-2-image-tv"></div>
+    </div>
+</section>
