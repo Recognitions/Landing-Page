13a22ce1fe52a6c9185ad3808ec114ab8984a156
devroseno <contatodanielroseno@gmail.com>
2022-12-03 19:20:02 -0300

    contact finished

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,86 @@
 <footer>
-    footer
+    <div class="site-footer" style="position:relative;">
+        <ul class="footer-links">
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Perguntas frequentes</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Central de Ajuda</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Conta</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Media Center</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Relações com investidores</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Carreiras</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Resgatar cartão pré-pago</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Comprar cartão pré-pago</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Formas de assistir</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Termos de Uso</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Privacidade</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Preferências de cookies</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Informações corporativas</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Entre em contato</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Teste de Velocidade</span>
+                </a>
+            </li>
+            <li class="footer-link-item">
+                <a href="#">
+                    <span>Avisos legais</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </footer>
--- pages/contato.php
+++ pages/contato.php
@@ -1,1 +1,60 @@
-contato
+<div class="global-content">
+    <div class="container">
+        <div class="left-pane">
+            <section class="title">
+                <h1>Contatar o atendimento ao cliente</h1>
+            </section>
+
+            <section class="content">
+                <h2>Ligue para nós usando o aplicativo Netflix</h2>
+
+                <p>
+                    Entrar em contato com a Netflix ficou mais fácil do que nunca.
+                    Agora é só usar o aplicativo Netflix no seu celular ou tablet Android ou iOS!
+                    Ligar pelo aplicativo é grátis! Você só precisa de uma conexão fixa ou móvel com a Internet.
+                </p>
+
+                <span class="play-store"></span>
+            </section>
+
+            <section class="content">
+                <h2>Ligue de qualquer telefone</h2>
+
+                <p>É necessário ter um serviço de telefonia fixa ou celular.</p>
+
+                <div class="phone-contact">
+                    <a href="#" class="btn-phone">Ligue para nós</a>
+                </div>
+            </section>
+
+            <section class="content">
+                <h2>Bate-papo</h2>
+
+                <p>É necessário ter conexão fixa ou móvel com a Internet.</p>
+
+                <div class="phone-contact">
+                    <a href="#" class="btn-phone">Inicie agora</a>
+                </div>
+            </section>
+        </div>
+        <div class="right-pane">
+            <div class="wrapper">
+                <section class="content-help">
+                    <p><strong>Dúvidas mais comuns</strong></p>
+
+                    <ul class="selfhelp-options">
+                        <li class="help-option"> <a href="#">Como faço para redefinir a minha senha?</a> </li>
+                        <li class="help-option"> <a href="#">Como faço para solicitar uma série ou filme?</a> </li>
+                        <li class="help-option"> <a href="#">Como faço para cancelar a minha conta?</a> </li>
+                        <li class="help-option"> <a href="#">Por que a Netflix não está funcionando?</a> </li>
+                        <li class="help-option"> <a href="#">O que é a Netflix?</a> </li>
+                    </ul>
+                </section>
+                <section class="content-search">
+                    <p><strong>Buscar outra resposta</strong></p>
+                    <input placeholder="BUSCAR"></inputr>
+                </section>
+            </div>
+        </div>
+    </div>
+</div>
