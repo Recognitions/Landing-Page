7e5bfbbf0e8776aaf6a5da77717a67284b70cba4
Recognitions <102226234+Recognitions@users.noreply.github.com>
2022-12-03 19:40:27 -0300

    cards dinamicos

--- pages/default.php
+++ pages/default.php
@@ -1,5 +1,24 @@
 <section class="default">
     <script>
+
+        const array = [
+            {
+                title: "Aproveite na TV.",
+                desc: "Assista em Smart TVs, PlayStation, Xbox,<br> Chromecast, Apple TV, aparelhos de Blu-ray e<br> outros dispositivos.",
+                image: "tv"
+            },
+            {
+                title: "Crie perfis para crianças.",
+                desc: "Deixe as crianças se aventurarem com seus <br>personagens favoritos em um espaço feito só <br>para elas, sem pagar a mais por isso.",
+                image: "child"
+            },
+            {
+                title: "Baixe séries para assistir offline.",
+                desc: "Disponível em todos os planos, exceto no Básico com anúncios.",
+                image: "st"
+            },
+        ]
+
         function sscroll(){
             window.scrollTo(0, document.body.scrollHeight);
         }
@@ -17,10 +36,25 @@
             }
         }
 
+        function replaceCard(){
+            const card = document.getElementById("card");
+            const arr = array[Math.floor(Math.random()*array.length)];
+            card.innerHTML=`
+                <div class="default-2-text">
+                    <h2>${arr.title}</h2>
+                    <label>${arr.desc}</label>
+                </div>
+                <div class="default-2-image">
+                    <div class="default-2-image-${arr.image}"></div>
+                </div>
+            `
+        }
+
         setTimeout(() => {
             animateText()
             setInterval(() => {
-                animateText()
+                animateText();
+                replaceCard();
             }, 2500);
         }, 500);
         
@@ -30,10 +64,7 @@
         </label>
     </span>
 </section>
-<section class="default-2">
-    <script>
-        
-    </script>
+<section class="default-2" id="card">
     <div class="default-2-text">
         <h2>Aproveite na TV.</h2>
         <label>Assista em Smart TVs, PlayStation, Xbox,<br> Chromecast, Apple TV, aparelhos de Blu-ray e<br> outros dispositivos.</label>
